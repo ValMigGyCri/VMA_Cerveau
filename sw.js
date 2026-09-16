f905309e0b3d7cbf213baaa4c6c0ba96eb2512bc
ValMigGyCri <valentin.mignone@eduvaud.ch>
2026-09-16 11:40:47 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+/* Stratégie : réseau d'abord, cache en secours.
+   En ligne, l'écran reçoit toujours la dernière version publiée.
+   Hors connexion, il rejoue la dernière version mise en cache. */
+
+const CACHE = 'vma-cerveau';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(k => Promise.all(k.filter(n => n !== CACHE).map(n => caches.delete(n))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
